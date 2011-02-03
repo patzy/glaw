32bf7d4c44de3dd7688659e1116e500b82147f4d
Morgan Veyret <patzy@appart.kicks-ass.net>
2011-02-03 14:56:36 +0100

    Added mesh struct and basic wavefront obj loader.

--- src/3d.lisp
+++ src/3d.lisp
@@ -146,3 +146,155 @@
 (defun 3d-view-look-at (v eye target)
   (basis-set-position (3d-view-basis v) eye)
   (3d-view-point-at v target))
+
+;;; Mesh
+(defstruct mesh
+  names
+  shapes
+  materials)
+
+(defun create-mesh (&optional (nb-parts 0))
+  (make-mesh :shapes (make-array nb-parts :adjustable t :fill-pointer t :element-type 'shape)
+             :materials (make-array nb-parts :adjustable t :fill-pointer t)
+             :names (make-array nb-parts :adjustable t :fill-pointer t :element-type 'string)))
+
+(defun mesh-nb-parts (mesh)
+  (length (mesh-shapes mesh)))
+
+(defun mesh-shape (mesh &optional (part-index (1- (length (mesh-shapes mesh)))))
+  (assert (< part-index (mesh-nb-parts mesh)))
+  (aref (mesh-shapes mesh) part-index))
+
+(defun mesh-material (mesh &optional (part-index (1- (length (mesh-shapes mesh)))))
+  (assert (< part-index (mesh-nb-parts mesh)))
+  (aref (mesh-materials mesh) part-index))
+
+(defun mesh-part (mesh &optional (part-index (1- (length (mesh-shapes mesh)))))
+  (values (mesh-shape mesh) (mesh-material mesh)))
+
+;; FIXME: use resource manager here
+(defun mesh-add-part (mesh &key (shape (create-shape 0 0)) (material +default-material+)
+                                (name (symbol-name (gensym "PART-"))))
+  (vector-push-extend shape (mesh-shapes mesh))
+  (vector-push-extend material (mesh-materials mesh)))
+
+(defun render-mesh (mesh)
+  (loop for i below (mesh-nb-parts mesh)
+       for shape = (mesh-shape mesh i)
+       for mat = (mesh-material mesh i)
+       do (set-material mat)
+          (render-shape shape)))
+
+
+;;; Wavefront OBJ :mesh asset
+;; http://www.martinreddy.net/gfx/3d/OBJ.spec
+(defasset :mesh '("obj")
+  ;; load
+  (lambda (filename)
+    (with-open-file (in filename :direction :input)
+      (let ((mesh (create-mesh)))
+        (loop for line = (read-line in nil)
+           while line
+           with group = nil
+           and material = +default-material+
+           and vertices = nil
+           and normals = nil
+           and tex-coords = nil
+           and vertex-indices = nil
+           and tex-vertex-indices = nil
+           and normal-indices = nil
+           when (string/= line "")
+           do (case (aref line 0)
+                (#\# (dformat "Wavefront comment: ~S~%" line))
+                (#\g (when group ;; store last group read
+                       (%mesh-add-wavefront-part mesh group material
+                                                 vertices tex-coords normals
+                                                 vertex-indices tex-vertex-indices normal-indices))
+                     (setf vertices nil
+                           tex-coords nil
+                           normals nil
+                           vertex-indices nil
+                           tex-vertex-indices nil
+                           normal-indices nil
+                           group (subseq line 2)))
+                (#\v (case (aref line 1)
+                       (#\t (unless tex-coords
+                              (setf tex-coords (make-array 0 :adjustable t :fill-pointer t)))
+                            (read-wavefront-vertex line tex-coords))
+                       (#\n (unless normals
+                              (setf normals (make-array 0 :adjustable t :fill-pointer t)))
+                            (read-wavefront-vertex line normals))
+                       (otherwise
+                        (unless group
+                          (setf group (symbol-name (gensym "MESH-"))))
+                        (unless vertices
+                          (setf vertices (make-array 0 :adjustable t :fill-pointer t)))
+                        (read-wavefront-vertex line vertices))))
+                (#\f (unless (or vertex-indices (not vertices))
+                       (setf vertex-indices (make-array 0 :adjustable t :fill-pointer t)))
+                     (unless (or normal-indices (not normals))
+                       (setf normal-indices (make-array 0 :adjustable t :fill-pointer t)))
+                     (unless (or tex-vertex-indices (not tex-coords))
+                       (setf tex-vertex-indices (make-array 0 :adjustable t :fill-pointer t)))
+                     (read-wavefront-face line vertex-indices tex-vertex-indices normal-indices))
+                (otherwise (dformat "Wavefront, ignoring ~S~%" line)))
+             finally (when group ;; store last group read
+                       (%mesh-add-wavefront-part mesh group material
+                                                 vertices tex-coords normals
+                                                 vertex-indices tex-vertex-indices normal-indices)))
+        mesh)))
+  ;; unload
+  (lambda (mesh)
+    (values)))
+
+(defun read-wavefront-vertex (line data)
+  (let ((datums (split-string (subseq line 2) #\Space)))
+    (loop for d in datums do (vector-push-extend (read-from-string d) data))))
+
+(defun read-wavefront-face (line vertex-indices tex-vertex-indices normal-indices)
+  (let ((datums (split-string (subseq line 2) #\Space)))
+    (loop for d in datums
+       do (let* ((indices (mapcar #'1- (mapcar #'read-from-string (split-string d #\/))))
+                 (types (case (+ (loop for c across d when (char= c #\/) sum 1)
+                                 (length indices))
+                          (1 :vertex)
+                          (3 :vertex-texture)
+                          (4 :vertex-normal)
+                          (5 :vertex-texture-normal))))
+           (case types
+             (:vertex (vector-push-extend (first indices) vertex-indices))
+             (:vertex-texture
+              (vector-push-extend (first indices) vertex-indices)
+              (vector-push-extend (second indices) tex-vertex-indices))
+             (:vertex-normal
+              (vector-push-extend (first indices) vertex-indices)
+              (vector-push-extend (second indices) normal-indices))
+             (:vertex-texture-normal
+              (vector-push-extend (first indices) vertex-indices)
+              (vector-push-extend (second indices) tex-vertex-indices)
+              (vector-push-extend (third indices) normal-indices)))))))
+
+(defun %mesh-add-wavefront-part (mesh name material vertices tex-coords normals
+                                 vertex-indices tex-vertex-indices normal-indices)
+  (let ((indices (make-array (length vertex-indices))))
+    (loop for i below (length vertex-indices)
+         do (setf (aref indices i) i))
+    (let ((shape (create-shape-from-arrays
+                  indices
+                  (%vertex-data-from-face-data vertices vertex-indices)
+                  nil
+                  (%vertex-data-from-face-data tex-coords tex-vertex-indices)
+                  (%vertex-data-from-face-data normals normal-indices))))
+      (unless (and normals (not (zerop (length normal-indices))))
+        (shape-compute-normals shape))
+      (mesh-add-part mesh :shape shape :material material :name name))))
+
+;; ;; Wavefront OBJ :material-lib asset
+;; (defasset :material-lib '("mtl")
+;;   ;; load
+;;   (lambda (filename)
+    
+;;   ;; unload
+;;   (lambda (material)
+;;     (declare (ignore material))
+;;     (values)))
